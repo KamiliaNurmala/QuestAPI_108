76cd0251c357feba36387cf81ec25f712e5d121d
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-12-25 17:48:46 +0700

    Mengimplementasikan basic scaffold and FAB for DetailSiswaScreen

--- app/src/main/java/com/example/praktikum10/view/HalamanDetail.kt
+++ app/src/main/java/com/example/praktikum10/view/HalamanDetail.kt
@@ -1,4 +1,55 @@
 package com.example.praktikum10.view
 
-class HalamanDetail {
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.statusUIDetail
+
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetail.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    val currentState = uiState
+                    if (currentState is StatusUIDetail.Success) {
+                        navigateToEditItem(currentState.statusiswa.id)
+                    }
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.edit_siswa),
+                )
+            }
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailDataSiswa(
+            statusUIDetail = uiState,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.hapusSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState()
+                )
+        )
+    }
 }
