aa65cf6e7ae24151681d8adb57041ef42df32a51
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-12-25 18:06:00 +0700

    Mengintegrasikan BodyDetailDataSiswa and delete logic to DetailSiswaScreen

--- app/src/main/java/com/example/praktikum10/view/HalamanDetail.kt
+++ app/src/main/java/com/example/praktikum10/view/HalamanDetail.kt
@@ -92,6 +92,49 @@ fun DetailDataSiswa(
 }
 
 @Composable
+private fun BodyDetailDataSiswa(
+    statusUIDetail: StatusUIDetail,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+        when (statusUIDetail) {
+            is StatusUIDetail.Success -> {
+                DetailDataSiswa(
+                    siswa = statusUIDetail.statusiswa,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            else -> { /* Handle Loading/Error */ }
+        }
+
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
+}
+
+@Composable
 private fun BarisDetailData(
     @StringRes labelResID: Int,
     itemDetail: String,
