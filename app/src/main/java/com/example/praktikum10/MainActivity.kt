bfafe5748594207268a845b80f4e635fb171732e
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-12-25 17:02:43 +0700

    Menambah top app bar dan MainActivity setup

---
+++ app/src/main/java/com/example/praktikum10/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.praktikum10
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.praktikum10.ui.theme.Praktikum10Theme
+import com.example.praktikum10.uicontroller.DataSiswaApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Praktikum10Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DataSiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
